cde9f42b177c2d5125d739fa780089e6ff0b8548
AkShAy21211 <akshay68580@gmail.com>
2025-01-15 12:17:22 +0530

    feat(model): added user schema

---
+++ models/user.js
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    googleId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("User", userSchema);
